d39778e9de5944d9d4a7f6df8ecb4ae1e4a22935
sokserey-hing <soksereihing@gmail.com>
2023-11-06 23:20:48 +0700

    13. Step 07 - Bringing in Spring Framework to Make Java App Loosely Coupled

--- src/main/java/com/serey/springboot/learnspringframework/AppGamingBasiscJava.java
+++ src/main/java/com/serey/springboot/learnspringframework/AppGamingBasiscJava.java
@@ -8,12 +8,12 @@ import com.serey.springboot.learnspringframework.game.SuperContraGame;
 public class AppGamingBasiscJava {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 //		var game = new MarioGame();
 //		var game = new SuperContraGame();
-		var game = new PacmanGame();
+		var game = new PacmanGame(); //1. Object creation 
 		
-		var gameRunner = new GameRunner(game);
+		var gameRunner = new GameRunner(game); //2. Object creation + wiring of dependencies 
+												// Game is a Dependency of GameRunner
 		gameRunner.run();
 
 	}
