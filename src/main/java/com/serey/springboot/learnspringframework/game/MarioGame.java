3cfcb4474506b6b04c58ccf8bfaaa965bf95adef
sokserey-hing <soksereihing@gmail.com>
2023-11-10 14:40:03 +0700

    26. Step 02 - Exploring Primary and Qualifier Annotations for Spring Components

--- src/main/java/com/serey/springboot/learnspringframework/game/MarioGame.java
+++ src/main/java/com/serey/springboot/learnspringframework/game/MarioGame.java
@@ -1,5 +1,10 @@
 package com.serey.springboot.learnspringframework.game;
 
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+@Component
+@Primary
 public class MarioGame implements GamingConsole{
 	
 	
--- src/main/java/com/serey/springboot/learnspringframework/game/SuperContraGame.java
+++ src/main/java/com/serey/springboot/learnspringframework/game/SuperContraGame.java
@@ -1,5 +1,10 @@
 package com.serey.springboot.learnspringframework.game;
 
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+@Component
+@Qualifier("SuperContraGameQualifier")
 public class SuperContraGame implements GamingConsole{
 	
 	public void up() {
